d1d669c44094333c6f34a767825974d4461f43ce
ScottShakaMwangi <scottshakamwangi@gmail.com>
2022-11-01 21:00:16 +0300

    modified .env and index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
-const app = express();
-const mangoose = require("mongoose")
+const app = express ();
+const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 mongoose
 .connect(process.env.MONGO_URL)
-.then(()=>console.log("DBConnection Succesfull!")).catch((err)=>{
-    console.log(err);
+.then(()=>console.log("DBconnection Successfull!"))
+.catch((err)=>{
+    console.log(err)
 });
 
-
-app.listen(process.env.PORT || 5000, () => {
-    console.log("The backend is running")
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Backend server is running!")
 })
