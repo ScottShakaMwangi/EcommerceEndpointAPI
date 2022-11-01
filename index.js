a6b3f12e438f57d183d57e40dd62eb466fc99645
ScottShakaMwangi <scottshakamwangi@gmail.com>
2022-11-01 14:13:06 +0300

    added .env file for hiding secret key

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const mangoose = require("mongoose")
+const dotenv = require("dotenv");
 
-mongoose.connect(
-    "mongodb+srv://ScottShaka:ScottShaka@cluster0.25sit4c.mongodb.net/Shop?retryWrites=true&w=majority"
-).then(()=>console.log("DBConnection Succesfull!")).catch((err)=>{
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>console.log("DBConnection Succesfull!")).catch((err)=>{
     console.log(err);
 });
 
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000, () => {
     console.log("The backend is running")
 })
