3cfa581d648814fb10d1e64438448f642aa738b6
ScottShakaMwangi <scottshakamwangi@gmail.com>
2022-11-01 13:48:11 +0300

    connected my mongoose server with vs code

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
+const mangoose = require("mongoose")
 
-
-
+mongoose.connect(
+    "mongodb+srv://ScottShaka:ScottShaka@cluster0.25sit4c.mongodb.net/Shop?retryWrites=true&w=majority"
+).then(()=>console.log("DBConnection Succesfull!")).catch((err)=>{
+    console.log(err);
+});
 
 
 app.listen(5000,()=>{
