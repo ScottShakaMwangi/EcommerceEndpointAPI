c9afa142603eca7ef5f9eea5d8423e561e329552
ScottShakaMwangi <scottshakamwangi@gmail.com>
2022-11-06 02:46:24 +0300

    changes in the index.js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require("express");
+import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express ();
-const mongoose = require("mongoose")
-const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const PORT = 5000;
 
-dotenv.config();
+app.use(bodyParser.json());
 
-mongoose
-.connect(process.env.MONGO_URL)
-.then(()=>console.log("DBconnection Successfull!"))
-.catch((err)=>{
-    console.log(err)
-});
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
 
-app.use("/api/user", userRoute);
 
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log("Backend server is running!")
-})
+
+
+
+
+
+
+// const dotenv = require("dotenv");
+// const userRoute = require("./routes/user")
+
+// dotenv.config();
+
+
+
+// app.use("/api/user", userRoute);
+
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 
-router.get("/usertest", (req,res)=>{
-res.send("user test is successfull")
-})
+// router.get("/usertest", (req,res)=>{
+// res.send("user test is successfull")
+// })
 
-lh:5000/api/user/usertest
+// lh:5000/api/user/usertest
 
-module.export = router
+// module.export = router
